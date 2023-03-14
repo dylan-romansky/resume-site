4361210481cde5dc9622f86c1d2698ac5dcc4c88
bigdiggysquiggle <dylanromansky@yahoo.com>
2023-03-14 14:39:46 -0600

    automation workaround in place

--- restart.sh
+++ restart.sh
@@ -5,4 +5,4 @@ RESUME="$(docker image ls | grep resume | awk '{print $3}')"
 sudo docker rmi -f "$RESUME"
 sudo docker compose build --no-cache
 sudo docker compose up -d
-sudo docker exec "$(docker container ls | grep cockroach | awk '{print $1}' | cut -d: -f2)" bash -c "cockroach sql --insecure < /docker-entrypoint-initdb.d/schema.sql"
+sudo docker exec "$(docker container ls | grep cockroach | awk '{print $1}' | cut -d: -f2)" bash -c "cockroach init --insecure; cockroach sql --insecure < /docker-entrypoint-initdb.d/schema.sql"
