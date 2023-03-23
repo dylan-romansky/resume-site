5c4e361de51586091b5e304af2970f57a722b190
bigdiggysquiggle <dylanromansky@yahoo.com>
2023-03-22 18:53:27 -0600

    renaming directories and cleaning up files for easier navigation

--- backend/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# A quick and easy shutdown and cleanup for the
-# application when deployed via Docker
-
-docker container rm -f "$(docker container ls | grep resume | awk '{print $1}' | cut -d: -f2)"
-docker rmi -f "$(docker image ls | grep resume | awk '{print $3}')"
