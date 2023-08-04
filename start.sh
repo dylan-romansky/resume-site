47d995519ba114dede83f3a240135b7b927d2dd0
bigdiggysquiggle <randomcouchpotato@gmail.com>
2023-08-04 17:30:52 -0600

    I need to not be using minikube

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+minikube stop
 minikube start
-kubectl exec cockroachdb-0 -- /cockroach/cockroachinit --certs-dir='/cockroach/cockroach-certs'
+eval $(minikube -p minikube docker-env)
+kubectl delete deployment resume-frontend
+kubectl delete deployment resume-backend
+
+while [ -z "$(kubectl get pods | grep Running)" ]; do
+	sleep 1
+done
+
+echo "$(kubectl get pods)"
+kubectl exec 'cockroachdb-0' -- /cockroach/cockroach init --certs-dir='/cockroach/cockroach-certs'
+kubectl apply -f yaml/backend.yaml
+kubectl apply -f yaml/frontend.yaml
+
+minikube tunnel
